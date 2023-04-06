1efbf10749b177207ebf6d284fb317da52658ab5
Raymond Morales <94730761+Raydevl@users.noreply.github.com>
2023-04-05 21:42:13 -0400

    added ability
    added the ability for chatGPT to write or something idk

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import openai
 import os
+import sqlite3
 
-openai.api_key = os.environ["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"]
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 def read_file(file_path):
     with open(file_path, "r") as f:
@@ -17,16 +18,43 @@ file_content = read_file(file_path)
 prompt = "The following is a text completion task:\n\n" + file_content
 
 response = openai.Completion.create(
-    engine="gpt-3.5-turbo",
+    engine="text-davinci-002",
     prompt=prompt,
-    max_tokens=2048,
+    max_tokens=1024,
     n=1,
     stop=None,
-    temperature=1.8,
+    temperature=0.5,
 )
 
 output = response.choices[0].text
 print(output)
 
 output_file_path = "output.txt"
-write_file(output_file_path, f"Input file path: {file_path}\nOutput file path: {output_file_path}\nPrompt: {prompt}\nEngine: gpt-3.5-turbo\nResponse: {output}")
+write_file(output_file_path, output)
+
+conn = sqlite3.connect('memory.db')
+cursor = conn.cursor()
+
+# Create a table to store the memory
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS memory (
+        id INTEGER PRIMARY KEY,
+        input_file_path TEXT,
+        output_file_path TEXT,
+        prompt TEXT,
+        engine TEXT,
+        response TEXT
+    );
+''')
+
+# Insert the relevant information into the memory table
+cursor.execute('''
+    INSERT INTO memory (input_file_path, output_file_path, prompt, engine, response)
+    VALUES (?, ?, ?, ?, ?);
+''', (file_path, output_file_path, prompt, 'text-davinci-002', output))
+
+# Commit the changes to the database
+conn.commit()
+
+# Close the database connection
+conn.close()
